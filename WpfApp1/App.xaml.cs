9fce7f5aefbd907e2efec18aa83354e2a4e5a0c0
Kamil Grabowski <kgrabowski1@vp.pl>
2023-10-11 22:14:01 +0200

    Change View

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfApp1.ViewModel;
 
 namespace WpfApp1
 {
@@ -13,5 +15,29 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        //private readonly ServiceProvider _serviceProvider;
+        //public App()
+        //{
+        //    IServiceCollection services = new ServiceCollection();
+        //    services.AddSingleton<MainWindow>(provider => new MainWindow
+        //    {
+        //        DataContext = provider.GetRequiredService<MainViewModel>() 
+        //    });
+        //    services.AddSingleton<MainViewModel>();
+        //    services.AddSingleton<HomeViewModel>();
+        //    services.AddSingleton<GameViewModel>();
+
+        //    _serviceProvider = services.BuildServiceProvider();
+        //}
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            base.OnStartup(e);
+        }
     }
 }
---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp1.View;
+using WpfApp1.ViewModel.BaseClass;
+
+namespace WpfApp1.ViewModel
+{
+    class MainViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private object currentView;
+        public MainViewModel()
+        {
+            currentView = new GameView();
+            changeView = new RelayCommand(changeViewFunc);
+        }
+
+        public ICommand changeView { get; set; }
+
+        public void changeViewFunc(object obj)
+        {
+            CurrentView = new HomeView();
+        }
+        private bool y = false;
+
+        public object CurrentView {
+            get
+            {
+                return currentView;
+            }
+            set
+            {
+                if (y == false)
+                {
+                    currentView = new HomeView();
+                    y = true;
+                }
+                else
+                {
+                    currentView = new GameView();
+                    this.y = false;
+                }
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+    
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
